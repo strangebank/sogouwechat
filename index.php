56235de6c39d7c17c48aaab033731aa7d3a028b6
strangebank <strangebank@gmail.com>
2015-11-01 13:20:23 +0800

    compose phpquery

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
-phpinfo();
+require 'vendor/autoload.php';
+
+
+phpQuery::newDocumentFile('http://www.helloweba.com/blog.html'); 
+$artlist = pq(".blog_li"); 
+foreach($artlist as $li){ 
+   echo pq($li)->find('h2')->html().""; 
+} 
