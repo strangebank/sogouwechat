c18b5488cde3e74de2073574bcbe0be4d44e145f
strangebank <strangebank@gmail.com>
2015-11-01 13:50:37 +0800

    change index

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 require 'vendor/autoload.php';
-
-
-phpQuery::newDocumentFile('http://www.helloweba.com/blog.html'); 
-$artlist = pq(".blog_li"); 
+$url = 'http://weixin.sogou.com/weixin?type=1&query=备孕&ie=utf8';
+phpQuery::browserGet($url);
+$artlist = pq(".wx-rb_v1 _item");
 foreach($artlist as $li){ 
    echo pq($li)->find('h2')->html().""; 
 } 
