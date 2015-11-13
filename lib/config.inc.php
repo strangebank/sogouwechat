1116b5aa6511181290b5ee592f81ce0382e9f2ee
strangebank <strangebank@gmail.com>
2015-11-13 22:38:33 +0800

    db info

--- lib/config.inc.php
+++ lib/config.inc.php
@@ -1,4 +1,5 @@
 <?php
 //http://w.sugg.sogou.com/sugg/ajaj_json.jsp?key=孕&type=wxpub&ori=yes&pr=web&abtestid=&ipn=
 /** @var TYPE_NAME $keywords */
-$keywords = array('备孕','胎教','母婴','怀孕');
+#$keywords = array('备孕','胎教','母婴','怀孕');
+$keywords = array('自由行','出境游');
--- lib/parsehtml.class.php
+++ lib/parsehtml.class.php
@@ -11,25 +11,41 @@ class parsehtml{
     }
     public function run(){
         echo $this->uri;
-        $this->html=curl_get_file_contents($this->uri);
+        #$this->html=getHtml($this->uri);
         echo $this->html;
-        $this->parseSearchList();
+        $this->parseSearchList($this->uri);
     }
-    private function parseSearchList(){
-        phpQuery::newDocumentHTML($this->html);
+    private function parseSearchList($uri){
+        $this->getHtml($this->uri);
         $artlist = pq(".wx-rb_v1");
         foreach($artlist as $li){
-            echo 'http://weixin.sogou.com'.pq($li)->attr('href')."     >>>";
-            echo pq($li)->find('.txt-box >h4>span')->html();
+            $url =  'http://weixin.sogou.com'.pq($li)->attr('href');
+            echo pq($li)->find('.txt-box >h4>span')->html()."\r\n";
+            $this->parseWechatList($url);
             die;
         }
 
     }
-    private function parseWechatList(){
-
+    private function parseWechatList($uri){
+        echo "Start:".$uri."\r\n";
+        $this->getHtml($uri);
+        $artlist = pq(".news_lst_tab");
+        var_dump($artlist);
+        foreach($artlist as $li){
+            echo pq($li)->attr('href')."\r\n";
+        }
     }
     private function parseWechatContents(){
 
+    }
+    private function getHtml($uri){
+        if(!$uri)return false;
+        $contents = curl_get_file_contents($uri);
+        if($contents){
+            return phpQuery::newDocumentHTML($contents);
+            ;
+        }
+        return '';
     }
     function __destruct()
     {
